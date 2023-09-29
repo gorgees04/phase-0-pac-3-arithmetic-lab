f717dd05be02f81f9c7c79ff1d3a9670f2d35d9e
Gorgees Odisho <gorgees041@gmail.com>
2023-09-29 06:36:35 +0300

    DONE WITH MATH

---
+++ index.js
@@ -0,0 +1,37 @@
+
+// basic math functions
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function increment(n) {
+    return n + 1
+}
+
+function decrement(n) {
+    return n - 1
+}
+
+//makeInt(n)
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+console.log(makeInt("hjfbj"))
+
+//preserveDecimal(n)
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
